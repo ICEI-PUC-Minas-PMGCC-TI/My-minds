d38e7bae429843d0e12b8a4e05fd42064b3a6809
JoaoFilardi <126689051+JoaoFilardi@users.noreply.github.com>
2023-07-02 20:22:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,75 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
-  
-    // Obter os valores dos campos
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var dob = document.getElementById('dob').value;
-    var cipcrm = document.getElementById('cipcrm').value;
-    var specialization = document.getElementById('specialization').value;
-    var password = document.getElementById('password').value;
-  
-    // Verificar o comprimento da senha
-    if (password.length >= 6 && password.length <= 16) {
-      // Criar um objeto com os dados cadastrados
-      var userData = {
-        name: name,
-        email: email,
-        dob: dob,
-        cipcrm: cipcrm,
-        specialization: specialization,
-        password: password
-      };
-  
-      // Armazenar os dados no Local Storage
-      localStorage.setItem('userData', JSON.stringify(userData));
-  
-      // Exibir uma mensagem de sucesso
-      alert('Cadastro realizado com sucesso!');
-    } else {
-      // Exibir uma mensagem de erro para o usuário
-      alert('A senha deve ter entre 6 e 16 dígitos.');
-    }
-  
-    // Redirecionar para outra página (opcional)
-    // window.location.href = 'outra_pagina.html';
+const reportList = document.getElementById("reportList");
+const toggleBtn = document.getElementById("toggleBtn");
+let reports = [
+  { title: "Relatório João Victor Filardi", link: "https://exemplo.com/relatorio1" },
+  { title: "Relatório Larrisa Maia", link: "https://exemplo.com/relatorio2" },
+  { title: "Relatório Carlos Silva", link: "https://exemplo.com/relatorio3" },
+  { title: "Relatório Laura Belo", link: "https://exemplo.com/relatorio4" },
+  { title: "Relatório Nelson Ferreira", link: "https://exemplo.com/relatorio5" },
+  { title: "Relatório Lucas Fagundes", link: "https://exemplo.com/relatorio6" }
+];
+let isReversed = false;
+
+function renderReports() {
+  reportList.innerHTML = "";
+  const sortedReports = isReversed ? [...reports].reverse() : reports;
+
+  sortedReports.forEach((report) => {
+    const li = document.createElement("li");
+    li.classList.add("report");
+
+    const title = document.createElement("a");
+    title.textContent = report.title;
+    title.href = report.link;
+    li.appendChild(title);
+
+    const date = document.createElement("span");
+    date.textContent = " Carregado em: " + new Date().toLocaleString(); // Adiciona a data e horário de carregamento
+    li.appendChild(date);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "&#128465;";
+    deleteBtn.addEventListener("click", () => {
+      showDeleteConfirmation(report);
+    });
+    li.appendChild(deleteBtn);
+
+    reportList.appendChild(li);
   });
-  
+}
+
+function showDeleteConfirmation(report) {
+  const confirmed = confirm("Tem certeza que deseja excluir o relatório?");
+  if (confirmed) {
+    deleteReport(report);
+  }
+}
+
+function deleteReport(report) {
+  reports = reports.filter((r) => r !== report);
+  renderReports();
+}
+
+toggleBtn.addEventListener("click", () => {
+  isReversed = !isReversed;
+  toggleBtn.innerHTML = isReversed ? '<i class="fas fa-chevron-up"></i>' : '<i class="fas fa-chevron-down"></i>';
+  renderReports();
+});
+
+renderReports();
+
+// Função para buscar relatórios da API e atribuir à variável "reports"
+function fetchReportsFromAPI() {
+  fetch("https://api.exemplo.com/relatorios")
+    .then((response) => response.json())
+    .then((data) => {
+      reports = data; // Atribui os relatórios retornados pela API à variável "reports"
+      renderReports(); // Exibe os relatórios na página
+    })
+    .catch((error) => {
+      console.log("Ocorreu um erro ao buscar os relatórios:", error);
+    });
+}
+
+// Chama a função para buscar relatórios da API ao carregar a página
+fetchReportsFromAPI();
